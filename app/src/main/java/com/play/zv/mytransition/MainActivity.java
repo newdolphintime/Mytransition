cb38e93f26661c1d2c022236372721fb14c8681f
zhangwei123 <1551583950@qq.com>
2017-05-02 23:28:20 +0800

    滑动动画

--- app/src/main/java/com/play/zv/mytransition/MainActivity.java
+++ app/src/main/java/com/play/zv/mytransition/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     private Button button1;
     private Button button2;
+    private View view1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,7 +56,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
             }
         });
-
+        view1 = findViewById(R.id.firstSharedView);
+        view1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Main4Activity.class);
+                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, view1, "sharedView").toBundle());
+            }
+        });
 
     }
 }
